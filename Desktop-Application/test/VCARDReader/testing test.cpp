33186b203ab07d086fe52230964021bdc811513c
Mohamed Moawia <mohamedmoawia4@gmail.com>
2020-12-26 14:48:47 +0200

    Change Project

--- Desktop-Application/test/VCARDReader/testing test.cpp
+++
@@ -1,8 +0,0 @@
-#include "gtest/gtest.h"
-#include "../../src/VCARDReader/VCARDReader.h"
-
-using namespace std;
-TEST(VCARDGenerator, testOfTheTest) {
-    VCARDReader g;
-    EXPECT_EQ(g.sayHello(), "Hello");
-}
